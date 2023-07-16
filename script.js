0dffe1e92bcb19b766bc542f7634c93b17fd12f4
nightelf1980 <rodrigo.pereiraarchiles@gmail.com>
2023-07-16 19:59:56 -0400

    Upload

---
+++ script.js
@@ -0,0 +1,160 @@
+function generateClientDatabase() {
+    var csvContent = "id,rut,nombre,apellido,fechaNacimiento,direccion,pais,ciudad,codigoPostal,telefono,correo,ocupacion,estadoCivil,centroCosto,promedioCompras\n";
+    var id = 1;
+
+    for (var i = 0; i < 5000; i++) {
+        var rut = generarNumeroIdentificacion();
+        var nombre = generarNombre();
+        var apellido = generarApellido();
+        var fechaNacimiento = generarFechaNacimiento();
+        var direccion = generarDireccion();
+        var pais = generarPais();
+        var ciudad = generarCiudad(pais);
+        var codigoPostal = generarCodigoPostal();
+        var telefono = generarTelefono();
+        var correo = generarCorreo(nombre, apellido);
+        var ocupacion = generarOcupacion();
+        var estadoCivil = generarEstadoCivil();
+        var centroCosto = generarCentroCosto();
+        var promedioCompras = generarPromedioCompras();
+
+        csvContent += id + "," + rut + "," + nombre + "," + apellido + "," + fechaNacimiento + "," + direccion + "," + pais + "," + ciudad + "," + codigoPostal + "," + telefono + "," + correo + "," + ocupacion + "," + estadoCivil + "," + centroCosto + "," + promedioCompras + "\n";
+        id++;
+    }
+  
+    return csvContent;
+  }
+
+  // Función para generar el número de identificación con guión
+function generarNumeroIdentificacion() {
+    var parteNumerica = generarNumeroAleatorio(9000000, 25000000);
+    var parteGuion = generarNumeroAleatorio(1, 9);
+    return parteNumerica + '-' + parteGuion;
+  }
+
+  // Función para generar un número aleatorio entre un rango dado, incluyendo los extremos
+function generarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function generarNombre() {
+    var nombres = ["Juan", "Carlos", "Pedro", "José", "Miguel", "Alejandro", "Tomás", "Jorge", "Alexis", "María", "Ana", "Luisa", "Laura", "Sofía", "Rafaela", "Patricia", "Carolina", "Alejandra"];
+    return nombres[Math.floor(Math.random() * nombres.length)];
+}
+
+function generarApellido() {
+    var apellidos = ["Gómez", "Rodríguez", "López", "Pérez", "Martínez", "González", "Hernández", "Fernández", "Díaz", "Torres", "Pereira", "Arévalo", "Solis", "Salas"];
+    return apellidos[Math.floor(Math.random() * apellidos.length)];
+}
+
+function generarFechaNacimiento() {
+    var year = Math.floor(Math.random() * 11) + 1980;
+    var month = Math.floor(Math.random() * 12) + 1;
+    var day = Math.floor(Math.random() * 28) + 1; // Considerando todos los meses con 28 días
+    return day + "/" + month + "/" + year;
+}
+
+function generarDireccion() {
+    var direcciones = ["Calle 1", "Calle 2", "Calle 3", "Calle 4","Calle 5", "Avenida 1", "Avenida 2", "Avenida 3", "Avenida 4", "Avenida 5"];
+    return direcciones[Math.floor(Math.random() * direcciones.length)];
+}
+
+function generarPais() {
+    var paises = ["Chile", "Argentina", "Perú", "México", "Colombia", "Ecuador", "Bolivia", "Uruguay", "Paraguay", "Venezuela"];
+    return paises[Math.floor(Math.random() * paises.length)];
+}
+
+function generarCiudad(pais) {
+    var ciudades = {
+        "Chile": ["Santiago", "Valparaíso", "Concepción", "Viña del Mar", "La Serena"],
+        "Argentina": ["Buenos Aires", "Córdoba", "Rosario", "Mendoza", "Mar del Plata"],
+        "Perú": ["Lima", "Arequipa", "Trujillo", "Cusco", "Piura"],
+        "México": ["Ciudad de México", "Guadalajara", "Monterrey", "Puebla", "Tijuana"],
+        "Colombia": ["Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena"],
+        "Ecuador": ["Quito", "Guayaquil", "Cuenca", "Santo Domingo", "Machala"],
+        "Bolivia": ["La Paz", "Santa Cruz", "Cochabamba", "Sucre", "Potosí"],
+        "Uruguay": ["Montevideo", "Salto", "Paysandú", "Maldonado", "Rivera"],
+        "Paraguay": ["Asunción", "Ciudad del Este", "San Lorenzo", "Capiatá", "Lambaré"],
+        "Venezuela": ["Caracas", "Maracaibo", "Valencia", "Barquisimeto", "Maracay"]
+    };
+    return ciudades[pais][Math.floor(Math.random() * ciudades[pais].length)];
+}
+
+function generarCodigoPostal() {
+    return Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+}
+
+function generarTelefono() {
+    var telefono = "";
+    for (var i = 0; i < 8; i++) {
+        telefono += Math.floor(Math.random() * 10);
+    }
+    return telefono;
+}
+
+function generarCorreo(nombre, apellido) {
+    var proveedores = ["gmail.com", "hotmail.com", "yahoo.com", "outlook.com", "aol.com"];
+    var nombreCorregido = nombre.toLowerCase();
+    var apellidoCorregido = apellido.toLowerCase();
+    return nombreCorregido + "." + apellidoCorregido + "@" + proveedores[Math.floor(Math.random() * proveedores.length)];
+}
+
+function generarOcupacion() {
+    var ocupaciones = ["Profesor(a)", "Ingeniero(a)", "Médico(a)", "Abogado(a)", "Trabajador(a) Social", "Analista", "Comunicador(a)", "Chef", "Artista", "Periodista", "Actor/Actriz", "Conductor(a)", "Escritor(a)"];
+    return ocupaciones[Math.floor(Math.random() * ocupaciones.length)];
+}
+
+function generarEstadoCivil() {
+    var estadosCivil = ["Soltero/a", "Casado/a", "Divorciado/a", "Viudo/a", "Unión Civil"];
+    return estadosCivil[Math.floor(Math.random() * estadosCivil.length)];
+}
+
+function generarCentroCosto() {
+    var letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numeros = "0123456789";
+    var centroCosto = "";
+
+    for (var i = 0; i < 2; i++) {
+        centroCosto += letras.charAt(Math.floor(Math.random() * letras.length));
+    }
+
+    for (var j = 0; j < 3; j++) {
+        centroCosto += numeros.charAt(Math.floor(Math.random() * numeros.length));
+    }
+
+    return centroCosto;
+}
+
+function generarPromedioCompras() {
+    return Math.floor(Math.random() * 10000001);
+}
+  
+  // Generar la base de datos de 1000 clientes en formato CSV
+  var clientDatabase = generateClientDatabase(5000);
+  
+  // Descargar el archivo CSV
+  function downloadCSV() {
+    var fileName = "client_database.csv";
+    var blob = new Blob([clientDatabase], { type: "text/csv;charset=utf-8;" });
+  
+    if (navigator.msSaveBlob) {
+      // IE 10+
+      navigator.msSaveBlob(blob, fileName);
+    } else {
+      // Otros navegadores
+      var link = document.createElement("a");
+      if (link.download !== undefined) {
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", fileName);
+        link.style.visibility = "hidden";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
+  }
+  
+  // Descargar el archivo CSV al hacer clic en el botón
+  var downloadButton = document.getElementById("downloadButton");
+  downloadButton.addEventListener("click", downloadCSV);
